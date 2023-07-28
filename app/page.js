6594f93c447cd18f942def76450332b6697b6e7b
ghulam-mustafa45 <139062950+ghulam-mustafa45@users.noreply.github.com>
2023-07-28 07:08:57 -0700

    changes

--- app/page.js
+++ app/page.js
@@ -1,113 +1,31 @@
 import Image from 'next/image'
+import { FaWindows} from 'react-icons/fa';
+import { FcGoogle} from "react-icons/fc";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+   <div className="flex flex-col items-center min-h-screen py-2  bg-gradient-to-r from-cyan-300 to-blue-700">
+  <img className='mt-4' src="https://me.classera.com/img/classera_logo_white.png?93c14775-d6a8-51df-97b2-7553b96c8787" alt="no image" />
+   
+   <div className='mt-16 bg-white rounded-t-md rounded-b-md w-96 pl-9'>
+    <input className=' outline-none border-b pl-2 pt-8 pb-2 mt-4 text-sm' type="text" placeholder='Login Name  '  /><hr />
+    <input className='pt-8 pb-2 pl-2 mt-3 text-sm' type="password" placeholder='passwords' /><hr />
+    <a className=' text-blue-400  text-xs font-bold mt-5 inline-block ' href="#"> Forgot Password ?</a>
+    <button className='bg-gradient-to-r from-cyan-300 to-blue-700 mb-14 text-white font-sans  hover:opacity-80 font-medium text-lg shadow-cyan-500/50 rounded border-s mt-5 w-11/12 pr-7 pl-7 pt-2 pb-2'> LOGIN</button>
+    <p className='w-11/12 text-center border-b-2 leading-{0.1em} text-slate-500 text-xs m'><span>Or Login With</span></p><br />
+   
+    <div className='flex justify-center mb-16'>
+      <a href="https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=df717cd3-94a4-4685-85b5-bb5c4fd9a48c&scope=User.Read%20openid%20profile%20offline_access&redirect_uri=https%3A%2F%2Fme.classera.com%2F&client-request-id=c024beee-4421-4de9-84b9-daf666ed1fbc&response_mode=fragment&response_type=code&x-client-SKU=msal.js.browser&x-client-VER=2.12.1&x-client-OS=&x-client-CPU=&client_info=1&code_challenge=OAmY6m5gpNboB9gnsAFIF6GPhpwRJXiPbRNae-kEhFM&code_challenge_method=S256&nonce=b7a660ca-c011-4db7-832f-3d812d744077&state=eyJpZCI6IjYxMWNjYTM5LTRlYzktNDQ3Ni1hZDA1LWMxMWIyNzQ2YWVmOSIsIm1ldGEiOnsiaW50ZXJhY3Rpb25UeXBlIjoicmVkaXJlY3QifX0%3D&sso_reload=true" className='border-2 border-gray-200 rounded-full p-2 mx-1'>
+        <FaWindows className='text-3xl'/>
+      </a>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <a href="https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fplus.login%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&state=72938b3f3357e6f44760ed0476733530&redirect_uri=https%3A%2F%2Fme.classera.com%2Fusers%2Fgoogle_login&response_type=code&client_id=351910993865-5t7sb3re5j9gbkv1vserb2djbef0ljud.apps.googleusercontent.com&access_type=offline&service=lso&o2v=1&flowName=GeneralOAuthFlow" className='border-2 border-gray-200 rounded-full p-2 mx-1'>
+        <FcGoogle className='text-3xl'/>
+      </a>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
+   </div>
+   <img className='mt-6 opacity-80 h-24' src="https://me.classera.com/img/learning-login.png?4b5b5048-7969-55df-afd8-97236b57e9f0" alt="no pic" />
+   </div>
   )
 }
